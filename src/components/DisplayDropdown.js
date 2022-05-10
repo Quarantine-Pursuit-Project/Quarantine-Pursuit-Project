a140db0420a1e4485cdf7cb9516e6a6259e32104
Harrison <harrison.hui.524@gmail.com>
2022-05-10 12:45:55 -0400

    saved category id value in a variable within the DisplayDropdown component to be used in API endpoint for questions generated

--- src/components/DisplayDropdown.js
+++ src/components/DisplayDropdown.js
@@ -1,18 +1,35 @@
+import { useState} from 'react';
+
 const DisplayDropdown = ({getCategory}) => {
+    const [userSelectedCategory, setUserSelectedCategory] = useState()
+
+    const userSelection = (e) => {
+        // we were told to save this "categorySelected" variable within state 
+        const categorySelected = e.target.value
+        setUserSelectedCategory(categorySelected)
+        console.log("The ID for this category is:", categorySelected)
+    }
 
     return (
         <section>
             <h2>This component is linked to Dropdown.js (and Dropdown.js) is a component that links to parent "App.js"</h2>
-            <select name="category" id="category">
-                <option value="placeholder" disabled>placeholder</option>
-                    {getCategory.map(category => {
-                            const { name, id } = category;
-                            return <option value={id}>{name}</option>
-                        })
-                    }
-            </select>
+            <form>
+                <select 
+                    defaultValue="gameCategory" 
+                    name="category" 
+                    id="category"
+                    onChange={userSelection}
+                >
+                    <option value="gameCategory" id="gameCategory" key="gameCategory" disabled>Pick a game category!</option>
+                        {getCategory.map(category => {
+                                const { name, id } = category;
+                                return <option value={id} id={id} key={id}>{name}</option>
+                            })
+                        }
+                </select>
+            </form>
+            <p>the category id is <span>{userSelectedCategory}</span> to be used for the API endpoint for the questions</p>
         </section>
-            
     )
 }
 
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,7 +5,7 @@ import DisplayDropdown from './DisplayDropdown';
 const Dropdown = () => {
     const [dropdown, setDropdown] = useState([])
 
-    // API call
+    // API call with category endpoint
     useEffect(() => {
         axios({
             url: "https://opentdb.com/api_category.php",
@@ -13,7 +13,6 @@ const Dropdown = () => {
             dataResponse: "json",
         }).then((response) => {
             const responseData = response.data.trivia_categories
-            // console.log(responseData)
             setDropdown(responseData)
         })
     }, [])
@@ -23,15 +22,6 @@ const Dropdown = () => {
             < DisplayDropdown 
                 getCategory={dropdown}
             />
-            
-            {/* <form action="">
-                <label htmlFor="category">Choose a category</label>
-
-                <select name="category" id="category">
-                    <option value="test">test</option>
-                    <option value="">{categoryName}</option>
-                </select>
-            </form> */}
         </div>
     )
 }
