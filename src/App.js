2ed9e233b85cdf538c96f120af584858ac2144f1
FJ400 <faheemandyjp@gmail.com>
2022-05-10 10:51:37 -0400

    added more text to h1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Dropdown from './components/Dropdown';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const [categories, Setcategories] = useState([])
+  const [categories, Setcategories] = useState([]);
 
-  
   return (
     <div className="App">
-      <h1>Quarantine Pursuit Project</h1>
-      < Dropdown />
+      <h1>Quarantine Pursuit Project!</h1>
+      <Dropdown />
     </div>
   );
 }
