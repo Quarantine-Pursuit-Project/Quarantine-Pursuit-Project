eb65b5b4e757ed0d5e2417416618c4ec97057dc7
Harrison <harrison.hui.524@gmail.com>
2022-05-09 21:58:04 -0400

    dropdown is properly displaying categories pulled from API

--- src/components/DisplayDropdown.js
+++ src/components/DisplayDropdown.js
@@ -1,28 +1,16 @@
 const DisplayDropdown = ({getCategory}) => {
-    // const categoryName = (getCategory) => {
-    //     getCategory.map
-    // }
 
     return (
         <section>
             <h2>This component is linked to Dropdown.js (and Dropdown.js) is a component that links to parent "App.js"</h2>
-            {
-                getCategory.map(getCategory => {
-                    // const categoryName = getCategory.name
-
-                    return(
-                        // step 1: dynamically add categories to dropdown
-                        <div>
-                            <select name="category" id="category">
-                                <option value="placeholder" disabled>placeholder</option>
-                                <option value={getCategory.name}>{getCategory.name}</option>
-                            </select>
-                        </div>
-
-                        // step 2: save whatever the user selects for Kyler's component
-                    )
-                })
-            }
+            <select name="category" id="category">
+                <option value="placeholder" disabled>placeholder</option>
+                    {getCategory.map(category => {
+                            const { name, id } = category;
+                            return <option value={id}>{name}</option>
+                        })
+                    }
+            </select>
         </section>
             
     )
